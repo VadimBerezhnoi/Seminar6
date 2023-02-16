1ac199bf75a6fe1ba737af46a26673d36d345a07
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-16 20:57:15 +0300

    Решена задача по выводу двухмерного массива по формуле

---
+++ Example_002/Example_002.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+int[,] GetArray(int row, int col, int minValue, int maxValue)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите размер строк ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите размер столбцов ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, col, -10, 10);
+PrintArray(array);
